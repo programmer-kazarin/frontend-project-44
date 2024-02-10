6adf4c3ff1a33ca37ab3891735475c400127fcd1
kazarin <kazarin.programmer@gmail.com>
2024-02-10 18:56:15 +0300

    fix in readName

--- games/cli.js
+++ games/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-const readName = () => readlineSync.question('May I have your name? ');
+const readName = () => readlineSync.question('May I have your name?');
 
 export default () => {
   console.log('Welcome to the Brain Games!');
