68fc6a3d8485c2aa36892dea0286632a42373b83
kazarin <kazarin.programmer@gmail.com>
2024-02-10 19:08:24 +0300

    fix greetings

--- games/cli.js
+++ games/cli.js
@@ -5,6 +5,6 @@ const readName = () => readlineSync.question('May I have your name?');
 export default () => {
   console.log('Welcome to the Brain Games!');
   const playerName = readName();
-  console.log(`Hello, ${playerName}!`);
+  console.log(`Hello, ${playerName}`);
   return playerName;
 };
