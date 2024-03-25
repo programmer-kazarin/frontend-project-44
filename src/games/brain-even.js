da5dcef88f483095d3d6d3d8d2b36435f7daf5c0
kazarin <kazarin.programmer@gmail.com>
2024-03-25 20:50:06 +0300

    refactoring: brain-even

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,27 +1,17 @@
-import greet from '../cli.js';
-import {
-  readAnswer, wrong, getRandomInt, playGame,
-} from '../index.js';
+import { playGame, } from '../index.js';
 
-const playOneRound = () => {
-  const random = getRandomInt(100, 1);
-  const isEven = random % 2 === 0;
-  console.log(`Question: ${random}`);
-  const answer = readAnswer();
-  if ((answer === 'yes' && isEven) || (answer === 'no' && !isEven)) {
-    console.log('Correct!');
-    return 1;
-  }
-  console.log(wrong(answer, isEven ? 'yes' : 'no'));
-  return 0;
-};
-
-const playEven = (playerName) => {
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  playGame(playOneRound, playerName);
-};
+const getRandomInt = (max, min = 0) => Math.floor(Math.random() * (max - min) + min);
 
 export default () => {
-  const playerName = greet();
-  playEven(playerName);
+  const questionsAndAnswers = [];
+  for (let i = 0; i < 3; i += 1) {
+    const random = getRandomInt(100, 1);
+    const round = {
+      question: random,
+      answer: random % 2 === 0 ? 'yes' : 'no',
+    }  
+    questionsAndAnswers.push(round);
+  }
+  
+  playGame('Answer "yes" if the number is even, otherwise answer "no".', questionsAndAnswers);  
 };
--- src/index.js
+++ src/index.js
@@ -1,28 +1,23 @@
 import readlineSync from 'readline-sync';
+import greet from './cli.js';
 
-const congrats = (playerName) => console.log(`Congratulations, ${playerName}!`);
-
-const readAnswer = () => readlineSync.question('Your answer: ');
-
-const wrong = (actual, expected) => `'${actual}' is wrong answer ;(. Correct answer was '${expected}'.`;
-
-const getRandomInt = (max, min = 0) => Math.floor(Math.random() * (max - min) + min);
-
-const TRIES = 3;
-
-const playGame = (gameFunc, playerName) => {
-  let rightAnswers = 0;
-  while (rightAnswers < TRIES) {
-    const result = gameFunc();
-    if (result === 0) {
+const playGame = (question, questionsAndAnswers) => {
+  const playerName = greet();
+  console.log(question);
+  for (const round of questionsAndAnswers) {
+    console.log(`Question: ${round.question}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (answer === round.answer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${round.answer}'.`);
       console.log(`Let's try again, ${playerName}!`);
       return;
     }
-    rightAnswers += 1;
   }
-  congrats(playerName);
+  console.log(`Congratulations, ${playerName}!`)
 };
 
 export {
-  congrats, readAnswer, wrong, getRandomInt, playGame,
+  playGame,
 };
