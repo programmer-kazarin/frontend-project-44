e9013fdf646f4a337ffcaf4d1d3fdf2bc5849c2c
kazarin <kazarin.programmer@gmail.com>
2024-03-25 21:08:09 +0300

    refactoring: brain-calc

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,54 +1,37 @@
 import { cons, car, cdr } from '@hexlet/pairs';
-import greet from '../cli.js';
-import {
-  congrats, readAnswer, wrong, getRandomInt,
-} from '../index.js';
+import { playGame, } from '../index.js';
 
-const calc = (pairOperands, operator, playerName) => {
-  console.log(`Question: ${car(pairOperands)} ${operator} ${cdr(pairOperands)}`);
-  let rightAnswer;
-  const answer = readAnswer();
+const getRandomInt = (max, min = 0) => Math.floor(Math.random() * (max - min) + min);
+
+const generate = (operator) => {
+  const pair = cons(getRandomInt(10), getRandomInt(10));
   switch (operator) {
     case '+':
-      rightAnswer = car(pairOperands) + cdr(pairOperands);
-      break;
+      return {
+        question: `${car(pair)} + ${cdr(pair)}`,
+        answer: `${car(pair) + cdr(pair)}`,
+      };
     case '-':
-      rightAnswer = car(pairOperands) - cdr(pairOperands);
-      break;
+      return {
+        question: `${car(pair)} - ${cdr(pair)}`,
+        answer: `${car(pair) - cdr(pair)}`,
+      };
     case '*':
-      rightAnswer = car(pairOperands) * cdr(pairOperands);
-      break;
+      return {
+        question: `${car(pair)} * ${cdr(pair)}`,
+        answer: `${car(pair) * cdr(pair)}`,
+      };
     default:
       console.error(`Invalid operator "${operator}"`);
       return false;
   }
-  if (parseInt(answer, 10) === rightAnswer) {
-    console.log('Correct!');
-    return true;
-  }
-  console.log(wrong(answer, rightAnswer));
-  console.log(`Let's try again, ${playerName}!`);
-  return false;
-};
-
-const playCalc = (playerName) => {
-  console.log('What is the result of the expression?');
-  const sumPair = cons(getRandomInt(100), getRandomInt(100));
-  if (calc(sumPair, '+', playerName) === false) {
-    return;
-  }
-  const subPair = cons(getRandomInt(100), getRandomInt(50));
-  if (calc(subPair, '-', playerName) === false) {
-    return;
-  }
-  const multPair = cons(getRandomInt(10), getRandomInt(10));
-  if (calc(multPair, '*', playerName) === false) {
-    return;
-  }
-  congrats(playerName);
 };
 
 export default () => {
-  const playerName = greet();
-  playCalc(playerName);
+  const questionsAndAnswers = [];
+  questionsAndAnswers.push(generate('+'));
+  questionsAndAnswers.push(generate('-'));
+  questionsAndAnswers.push(generate('*'));
+
+  playGame('What is the result of the expression?', questionsAndAnswers);
 };
