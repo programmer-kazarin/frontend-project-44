52b53f84d5d2ab657b2aa7643399faf0930ec3e5
kazarin <kazarin.programmer@gmail.com>
2024-03-27 23:21:14 +0300

    brain-calc refactoring

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,28 +1,33 @@
-import { cons, car, cdr } from '@hexlet/pairs';
 import playGame from '../index.js';
 
 const getRandomInt = (max, min = 0) => Math.floor(Math.random() * (max - min) + min);
 
+const calculate = (first, second, operation) => {
+  if (operation === '+') {
+    return first + second;
+  }
+  if (operation === '-') {
+    return first - second;
+  }
+  if (operation === '*') {
+    return first * second;
+  }
+  return NaN;
+};
+
+const generateQuestion = (first, second, operation) => {
+  const fullOperation = `${first} ${operation} ${second}`;
+  return {
+    question: fullOperation,
+    answer: `${calculate(first, second, operation)}`,
+  };
+};
+
 export default () => {
   const questionsAndAnswers = [];
-  const addPair = cons(getRandomInt(100), getRandomInt(100));
-  const addQuestionAndAnswer = {
-    question: `${car(addPair)} + ${cdr(addPair)}`,
-    answer: `${car(addPair) + cdr(addPair)}`,
-  };
-  questionsAndAnswers.push(addQuestionAndAnswer);
-  const subPair = cons(getRandomInt(100), getRandomInt(100));
-  const subQuestionAndAnswer = {
-    question: `${car(subPair)} - ${cdr(subPair)}`,
-    answer: `${car(subPair) - cdr(subPair)}`,
-  };
-  questionsAndAnswers.push(subQuestionAndAnswer);
-  const multPair = cons(getRandomInt(10), getRandomInt(10));
-  const multQuestionAndAnswer = {
-    question: `${car(multPair)} * ${cdr(multPair)}`,
-    answer: `${car(multPair) * cdr(multPair)}`,
-  };
-  questionsAndAnswers.push(multQuestionAndAnswer);
+  questionsAndAnswers.push(generateQuestion(getRandomInt(100), getRandomInt(100), '+'));
+  questionsAndAnswers.push(generateQuestion(getRandomInt(100), getRandomInt(100), '-'));
+  questionsAndAnswers.push(generateQuestion(getRandomInt(10), getRandomInt(10), '*'));
 
   playGame('What is the result of the expression?', questionsAndAnswers);
 };
